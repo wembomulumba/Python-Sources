1d6c85b3f85ac13f37372e38bafc253fdbf2e2c0
Wembo Mulumba <fabriceotepa@hotmail.com>
2019-05-13 07:14:51 +0200

    Create readwritecsv.py

---
+++ readwritecsv.py
@@ -0,0 +1,23 @@
+import datetime as dt      
+import matplotlib.pyplot as plt  
+from matplotlib import style   
+
+import pandas as pd      
+import pandas_datareader.data as web
+
+style.use('ggplot')
+
+start = dt.datetime(2017,1,1)
+end = dt.datetime(2018,12,31)
+
+df = web.DataReader('TSLA', 'yahoo', start, end)
+
+
+
+#print(df.head(4))
+#df.to_csv('tsla.csv')
+
+# to read a csv
+df = pd.read_csv('tsla.csv')
+print(df.head(6))
+
