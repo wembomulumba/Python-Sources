9decbc39c4d9a4b0f6231b5a6b3304c95beca90c
Wembo Mulumba <fabriceotepa@hotmail.com>
2019-05-16 21:17:51 +0200

    Create tcpClient.py

---
+++ networking/tcpClient.py
@@ -0,0 +1,20 @@
+#TCP Client Using Python3
+import socket       
+
+def Main():
+    host = '127.0.0.1'
+    port = 5000
+
+    s = socket.socket()
+    s.connect((host, port))
+
+    message = input("->")
+    while message != 'q':
+        s.send(message)
+        data = s.recv(1024)
+        print ('Received from Server: ', str(data))
+        message = input("->")
+    s.close()
+
+if __name__ == '__main__':
+    Main()        
