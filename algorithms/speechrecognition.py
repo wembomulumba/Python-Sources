d2cd0f0a4cca41f5671654e3f5c14bdad744f04e
Wembo Mulumba <fabriceotepa@hotmail.com>
2019-05-18 17:56:27 +0200

    updated speachrecognition.py

--- algorithms/speechrecognition.py
+++ algorithms/speechrecognition.py
@@ -9,3 +9,12 @@
 import speech_recognition as sr
 for index, name in enumerate(sr.Microphone.list_microphone_names()):
     print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
+  
+#The expected Output
+#Microphone with name "HDA Intel HDMI: 0 (hw:0,3)" found for `Microphone(device_index=0)`
+#Microphone with name "HDA Intel HDMI: 1 (hw:0,7)" found for `Microphone(device_index=1)`
+#Microphone with name "HDA Intel HDMI: 2 (hw:0,8)" found for `Microphone(device_index=2)`
+#Microphone with name "Blue Snowball: USB Audio (hw:1,0)" found for `Microphone(device_index=3)`
+#Microphone with name "hdmi" found for `Microphone(device_index=4)`
+#Microphone with name "pulse" found for `Microphone(device_index=5)`
+#Microphone with name "default" found for `Microphone(device_index=6)`    
