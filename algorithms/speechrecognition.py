f023234fdffe61a06c8c342d4ab12f62127e82a5
Wembo Mulumba <fabriceotepa@hotmail.com>
2019-05-18 17:51:47 +0200

    Update speechrecognition.py

--- algorithms/speechrecognition.py
+++ algorithms/speechrecognition.py
@@ -1,3 +1,11 @@
+
+#library for performing speech recognition, with support for several engines and APIs, online and offline.
+#Speech recognition engine/API
+
+#Quickstart: pip install SpeechRecognition. See the “Installing” section for more details.
+
+#To quickly try it out, run python -m speech_recognition after installing.
+
 import speech_recognition as sr
 for index, name in enumerate(sr.Microphone.list_microphone_names()):
     print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
